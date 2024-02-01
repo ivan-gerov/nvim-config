e9ba3cbb11c65c3896c14b79dace01b6ef1f0492
Ivan Gerov <ivan.gerov@iqvia.com>
2024-02-01 12:12:40 +0000

    Initial commit

---
+++ init.lua
@@ -0,0 +1,48 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require 'config.options'
+require 'config.keymaps'
+require 'config.autocmds'
+require('lazy').setup('plugins',{
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+  install = {
+    colorscheme = {'catppuccin'},
+  },
+  checker = { enabled = true },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        'gzip',
+        -- "matchit",
+        -- "matchparen",
+        'netrwPlugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+      },
+     },
+   }
+}, {})
+-- vim: ts=2 sts=2 sw=2 et
